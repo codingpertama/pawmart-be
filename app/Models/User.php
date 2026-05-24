9a5664a1b512c5b127bc1294ac4461eb4591846e
codingpertama <rafa.sugarda@gmail.com>
2026-05-24 18:26:43 +0700

    setup JWT & migrate

---
+++ app/Models/User.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Override;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
+
+#[Fillable(['name', 'email', 'password', 'role', 'phone'])]
+#[Hidden(['password', 'remember_token'])]
+class User extends Authenticatable implements JWTSubject
+{
+    /** @use HasFactory<UserFactory> */
+    use HasFactory, Notifiable;
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    // wajib untuk JWT
+    #[Override]
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    // wajib untuk JWT
+    #[Override]
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    // relasi ke orders
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    // relasi ke carts
+    public function carts()
+    {
+        return $this->hasMany(Cart::class);
+    }
+}
